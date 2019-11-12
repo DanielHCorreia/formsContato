fb5c900ebc7110f2c1a0cc1c11fa815ea048df26
alecsandros <ascj@cin.ufpe.br>
2019-11-12 14:22:54 -0300

    Adicionar template

--- sendEmail/settings.py
+++ sendEmail/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'sendEmail.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS':  [os.path.join(BASE_DIR, 'sendEmail/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
